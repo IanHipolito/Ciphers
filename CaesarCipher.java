db3f3b0e00213aa13274f0a6df7d9fd1027fd163
IanHipolito <ian.hipolito04@gmail.com>
2024-09-25 19:20:05 +0100

    Create CaesarCipher.java
    Code to encrypt uppercase and lowercase characters and append non-alphabetic characters without changing

---
+++ CaesarCipher.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CaesarCipher {
+    public static String encrypt(String text, int shift) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < text.length(); i++) {
+            char character = text.charAt(i);
+
+            if (Character.isUpperCase(character)) {
+                char ch = (char)(((int) character + shift - 65) % 26 + 65);
+                result.append(ch);
+            } else if (Character.isLowerCase(character)) {
+                char ch = (char)(((int) character + shift - 97) % 26 + 97);
+                result.append(ch);
+            } else {
+                result.append(character);
+            }
+        }
+        return result.toString();
+    }
+}
